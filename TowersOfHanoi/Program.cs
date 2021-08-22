18be2c4165ee5c05b80075fc3ce88f43f93a2a9c
Alina Drahos <87536523+ADrahos@users.noreply.github.com>
2021-08-22 15:36:12 -0500

    Initial Commit

---
+++ TowersOfHanoi/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace TowersOfHanoi
+{
+    class Program
+    {
+
+        static Stack<int> one = new Stack<int>();
+        static Stack<int> two = new Stack<int>();
+        static Stack<int> three = new Stack<int>();
+
+        static void Main(string[] args)
+        {
+            one.Push(3);
+            one.Push(2);
+            one.Push(1);
+
+            Move(one, two, three, 3);
+        }
+
+        public static void Move(Stack<int> origin, Stack<int> temp, Stack<int> destination, int count)
+        {
+            PrintAll();
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+            if (count == 0)
+            {
+                return;
+            }
+
+            Move(origin, destination, temp, count-1);
+
+            int disc = origin.Pop();
+            destination.Push(disc);
+
+            Move(temp, origin, destination, count - 1);
+
+            
+        }
+
+        public static void PrintAll()
+        {
+            Console.Write("one: ");
+            PrintStack(one);
+
+            Console.WriteLine("");
+
+            Console.Write("two: ");
+            PrintStack(two);
+
+            Console.WriteLine("");
+
+            Console.Write("three: ");
+            PrintStack(three);
+
+            Console.WriteLine("");
+        }
+
+        public static void PrintStack(Stack<int> stack)
+        {
+            foreach (var item in stack)
+            {
+                Console.Write(item + ",");
+            }
+        }
+    }
+}
